bffa0ea4736f079989790d7e7fcf7db72e78656d
jchojnacka <jechojnacka@gmail.com>
2024-08-14 00:19:45 +0200

    Styles changes and affirmation wheel added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import affirmations from './affirmations';
 
 function App() {
+  const getRandomAffirmation = () => {
+    const randomIndex = Math.floor(Math.random() * affirmations.length);
+    return affirmations[randomIndex];
+  };
+
+  const [currentAffirmation, setCurrentAffirmation] = useState(getRandomAffirmation());
+
+  const handleLogoClick = () => {
+    const newAffirmation = getRandomAffirmation();
+    setCurrentAffirmation(newAffirmation);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <div className="logo-title">Positivity Potion</div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='affirmation'><p>{currentAffirmation}</p></div>
+        <div class="frame">
+          <button className="custom-btn btn-3" onClick={handleLogoClick}><span>🪄 New Sparkle</span></button>
+        </div>
       </header>
     </div>
   );
---
+++ src/affirmations.js
@@ -0,0 +1,23 @@
+const affirmations = [
+    "You are capable of achieving great things",
+    "Every day is a new opportunity to grow",
+    "You are strong and resilient",
+    "Believe in yourself and all that you are",
+    "You have the power to create change",
+    "You are enough",
+    "You are worthy of love and happiness",
+    "You are deserving of respect and kindness",
+    "You are capable of overcoming any challenge",
+    "Your potential is limitless",
+    "You are surrounded by love and support",
+    "You are making a positive impact in the world",
+    "You are constantly growing and evolving",
+    "You are in control of your own happiness",
+    "You are worthy of success and abundance",
+    "You are beautiful inside and out",
+    "You are attracting positive experiences into your life",
+    "You are manifesting your dreams into reality",
+    "You are a survivor",
+  ];
+  
+  export default affirmations;
